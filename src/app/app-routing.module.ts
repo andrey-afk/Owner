fca89c4ebb7ec80816f4ce6c6b04868afa718ae2
andrey-afk <andrey-kama46@gmail.com>
2021-04-27 12:52:58 +0300

    E

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {UserListComponent} from "./components/user-list/user-list.component";
+import {UserInfoComponent} from "./components/user-info/user-info.component";
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path: '', component: UserListComponent},
+  {path: 'edit/:id', component: UserInfoComponent},
+  {path: 'add', component: UserInfoComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
